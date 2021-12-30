53efd6fc6b5adbd24dc15edefa07aa053062caed
Seongjun Kim <66054861+sjwhole@users.noreply.github.com>
2021-12-30 18:24:06 +0900

    Feature/contact (#2)
    * feat: Change Phone layout to horizontal
    
    * feat: Add READ_CONTACTS permission
    
    * feat: Add READ_CONTACTS permission - MainActivity
    
    * feat: Add contacts read from local storage
    
    * feat: Change Text size & bold & gravity
    
    * feat: Remove Id display on view
    
    * feat: getItem by newInstance method
    
    * feat: Add newInstance method to PhoneFragment

--- app/src/main/java/com/flowcamp/tab/MainActivity.java
+++ app/src/main/java/com/flowcamp/tab/MainActivity.java
@@ -1,48 +1,48 @@
 package com.flowcamp.tab;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.core.app.ActivityCompat;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 
 import com.flowcamp.tab.ui.main.SectionsPagerAdapter;
 import com.flowcamp.tab.databinding.ActivityMainBinding;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private SectionsPagerAdapter sectionsPagerAdapter;
+    private static final int READ_CONTACTS_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS},
+                    READ_CONTACTS_CODE);
+        }
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-//        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = binding.viewPager;
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = binding.fab;
 
-        /*FloatingActionButton fab = binding.fab;
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
     }
 }
