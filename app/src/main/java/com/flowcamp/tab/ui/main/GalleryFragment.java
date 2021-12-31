141653e760e2f17f577e6f69de69085647841f46
Sanu7D0 <83103105+Sanu7D0@users.noreply.github.com>
2021-12-31 16:32:59 +0900

    size variation

--- app/src/main/java/com/flowcamp/tab/ui/main/GalleryFragment.java
+++ app/src/main/java/com/flowcamp/tab/ui/main/GalleryFragment.java
@@ -1,5 +1,6 @@
 package com.flowcamp.tab.ui.main;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -18,14 +19,19 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
 import com.flowcamp.tab.R;
 import com.flowcamp.tab.databinding.FragmentPhoneBinding;
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class GalleryFragment extends Fragment {
     private static final String ARG_SECTION_NUMBER = "section_number";
@@ -80,7 +86,7 @@ public class GalleryFragment extends Fragment {
         // 스크린 사이즈 구하기
         int SCREEN_WIDTH = getContext().getResources().getDisplayMetrics().widthPixels;
 
-        ArrayList<String> imageList = loadImages(rootView);
+        Queue<String> imageList = loadImages(rootView);
 
         showImages(rootView, imageList);
 
@@ -89,11 +95,11 @@ public class GalleryFragment extends Fragment {
     }
 
     // load all images from gallery
-    private ArrayList<String> loadImages(View rootView) {
+    private Queue<String> loadImages(View rootView) {
         Uri uri;
         Cursor cursor;
         int column_index_data, column_index_folder_name;
-        ArrayList<String> imageList = new ArrayList<String>();
+        Queue<String> imageList = new LinkedList<>();
         String absolutePathOfImage = null;
         uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
 
@@ -116,57 +122,172 @@ public class GalleryFragment extends Fragment {
         return imageList;
     }
 
-    private void showImages(View rootView, ArrayList<String> imageList) {
+    private void showImages(View rootView, Queue<String> imageList) {
+        if (imageList.size() == 0) {
+            return;
+        }
+
+        int SCREEN_WIDTH = getContext().getResources().getDisplayMetrics().widthPixels;
+        int MARGIN = SCREEN_WIDTH / 100;
+        int MAX_WIDTH = SCREEN_WIDTH - 2*MARGIN;
+
         TableLayout tableLayout = rootView.findViewById(R.id.tableLayout);
+        TableRow row = makeRow(getActivity());
 
-        int COLUMN_SIZE = 3;
-        int i;
-        TableRow row = null;
-        for (i = 0; i < imageList.size(); i++) {
-            if (i % COLUMN_SIZE == 0) {
-                row = new TableRow(getActivity());
-            }
+        while (imageList.size() > 0) {
+            String currentPath = imageList.poll();
+            Bitmap current = BitmapFactory.decodeFile(currentPath);
+            double currentRatio = (double) current.getHeight() / current.getWidth();
 
-            ImageView iv = makeImageView(imageList.get(i), COLUMN_SIZE);
-            row.addView(iv);
-            if (i % COLUMN_SIZE == COLUMN_SIZE-1) {
-                // TableRow를 TableRayout에 추가
+            String nextPath = imageList.poll();
+            Bitmap next = BitmapFactory.decodeFile(nextPath);
+            if (next == null) {
+                // end
+                ResizeAndAddBitmap(current,
+                        MAX_WIDTH,
+                        (int) (current.getHeight() * ((double) MAX_WIDTH/current.getWidth())),
+                        row, MARGIN, MARGIN,
+                        currentPath);
                 tableLayout.addView(row);
+                break;
+            }
+            double nextRatio = (double)next.getHeight() / next.getWidth();
+
+            if (nextRatio > (double) 16/9) {
+                // end, end
+                ResizeAndAddBitmap(current,
+                        MAX_WIDTH,
+                        (int) (current.getHeight() * ((double) MAX_WIDTH/current.getWidth())),
+                        row, MARGIN, MARGIN,
+                        currentPath);
+                tableLayout.addView(row);
+
+                row = makeRow(getActivity());
+                ResizeAndAddBitmap(next,
+                        MAX_WIDTH,
+                        (int) (next.getHeight() * ((double) MAX_WIDTH/next.getWidth())),
+                        row, MARGIN, MARGIN,
+                        nextPath);
+                tableLayout.addView(row);
+
+                row = makeRow(getActivity());
+            }
+            else if (currentRatio + nextRatio > 2) {
+                // add, end
+
+                // W1 + W2 = Wm
+                // H1 = H2
+                // => H = (Wm * R1 * R2) / (R1 + R2)
+                // => W1 = H/R1
+                double height = ((MAX_WIDTH-MARGIN) * currentRatio * nextRatio) / (currentRatio + nextRatio);
+
+                ResizeAndAddBitmap(current,
+                        (int)(height / currentRatio),
+                        (int)height,
+                        row, MARGIN, MARGIN,
+                        currentPath);
+
+                ResizeAndAddBitmap(next,
+                        (int)(height / nextRatio),
+                        (int)height,
+                        row, 0, MARGIN,
+                        nextPath);
+
+                tableLayout.addView(row);
+                row = makeRow(getActivity());
+            }
+            // currentRatio + nextRatio <= 2
+            else {
+                // add, continue
+                if (imageList.peek() == null) {
+                    // end
+                    double height = ((MAX_WIDTH-MARGIN) * currentRatio * nextRatio) / (currentRatio + nextRatio);
+
+                    ResizeAndAddBitmap(current,
+                            (int)(height / currentRatio),
+                            (int)height,
+                            row, MARGIN, MARGIN,
+                            currentPath);
+
+                    ResizeAndAddBitmap(next,
+                            (int)(height / nextRatio),
+                            (int)height,
+                            row, 0, MARGIN,
+                            nextPath);
+
+                    tableLayout.addView(row);
+                }
+
+                String next2Path = imageList.poll();
+                Bitmap next2 = BitmapFactory.decodeFile(next2Path);
+                double next2Ratio = (double) next2.getHeight() / next2.getWidth();
+
+                // next2가  너무 길면 뒤로 빼기
+                if (next2Ratio > 2) {
+                    ResizeAndAddBitmap(next2,
+                            MAX_WIDTH,
+                            (int) (next2.getHeight() * ((double) MAX_WIDTH/next2.getWidth())),
+                            row, MARGIN, MARGIN,
+                            next2Path);
+
+                    tableLayout.addView(row);
+                    row = makeRow(getActivity());
+                }
+                else {
+                    // add, end
+                    double height = ((MAX_WIDTH - 2*MARGIN) * currentRatio * nextRatio * next2Ratio) /
+                            (currentRatio*next2Ratio + nextRatio*next2Ratio + currentRatio*nextRatio);
+
+                    ResizeAndAddBitmap(current,
+                            (int)(height / currentRatio),
+                            (int)height,
+                            row, MARGIN, MARGIN,
+                            currentPath);
+
+                    ResizeAndAddBitmap(next,
+                            (int)(height / nextRatio),
+                            (int)height,
+                            row, MARGIN, MARGIN,
+                            nextPath);
+
+                    ResizeAndAddBitmap(next2,
+                            (int)(height / next2Ratio),
+                            (int)height,
+                            row, MARGIN, MARGIN,
+                            next2Path);
+
+                    tableLayout.addView(row);
+                    row = makeRow(getActivity());
+                }
             }
         }
     }
 
-    private ImageView makeImageView(String imagePath, int columnSize) {
-        int SCREEN_WIDTH = getContext().getResources().getDisplayMetrics().widthPixels;
-        int MARGIN = SCREEN_WIDTH / 100;
-        int IMAGE_WIDTH = (SCREEN_WIDTH - 2*columnSize*MARGIN) / columnSize;
-        int IMAGE_HEIGHT = IMAGE_WIDTH;
+    private TableRow makeRow(Context context) {
+        TableRow row = new TableRow(context);
+        row.setLayoutParams(new TableRow.LayoutParams(
+                TableRow.LayoutParams.MATCH_PARENT,
+                TableRow.LayoutParams.WRAP_CONTENT));
+        return row;
+    }
 
+    private void ResizeAndAddBitmap(Bitmap bitmap, int width, int height, TableRow row, int marginLeft, int marginRight, String imagePath) {
+        bitmap = Bitmap.createScaledBitmap(bitmap, width, height, false);
+        ImageView iv = makeImageView(bitmap, marginLeft, marginRight, imagePath);
+        row.addView(iv);
+    }
+
+    private ImageView makeImageView(Bitmap bitmap, int marginLeft, int marginRight, String imagePath) {
         ImageView iv = new ImageView(getActivity());
-        iv.setScaleType(ImageView.ScaleType.CENTER_CROP); // 크기에 맞게 자르는 type
-
-        Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
-
-        // bitmap resize
-        int resizeWidth, resizeHeight;
-        if (bitmap.getWidth() >= bitmap.getHeight()) {
-            resizeWidth = (int)(bitmap.getWidth() * ((double)IMAGE_HEIGHT/(double)bitmap.getHeight()));
-            resizeHeight = IMAGE_HEIGHT;
-            bitmap = Bitmap.createScaledBitmap(bitmap, resizeWidth, resizeHeight,false);
-        }
-        else {
-            resizeWidth = IMAGE_WIDTH;
-            resizeHeight = (int)(bitmap.getHeight() * ((double)IMAGE_WIDTH/bitmap.getWidth()));
-            bitmap = Bitmap.createScaledBitmap(bitmap, resizeWidth, resizeHeight, false);
-        }
+//        iv.setScaleType(ImageView.ScaleType.CENTER_CROP); // 크기에 맞게 자르는 type
 
         iv.setImageBitmap(bitmap);
 
         // image 디자인
-        iv.setLayoutParams(new TableRow.LayoutParams(IMAGE_WIDTH, IMAGE_HEIGHT));
+        iv.setLayoutParams(new TableRow.LayoutParams(bitmap.getWidth(), bitmap.getHeight()));
 
         TableRow.LayoutParams lp = (TableRow.LayoutParams) iv.getLayoutParams();
-        lp.setMargins(MARGIN, MARGIN, MARGIN, MARGIN);
+        lp.setMargins(marginLeft, 0, marginRight, marginRight);
 
         // image 기능
         iv.setOnClickListener(new View.OnClickListener() {
