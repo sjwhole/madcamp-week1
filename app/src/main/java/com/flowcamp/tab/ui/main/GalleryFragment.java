6273121ed708670bae8810971bd9c92bce78fc3d
Sanu7D0 <83103105+Sanu7D0@users.noreply.github.com>
2022-01-03 22:39:57 +0900

    feat: Add gallery async 2

--- app/src/main/java/com/flowcamp/tab/ui/main/GalleryFragment.java
+++ app/src/main/java/com/flowcamp/tab/ui/main/GalleryFragment.java
@@ -20,7 +20,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewStub;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
