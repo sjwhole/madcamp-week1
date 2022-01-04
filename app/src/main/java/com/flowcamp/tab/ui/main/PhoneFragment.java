df9503e1b110d9fc01e2f0b2baa58318bf7ca2ac
Sanu7D0 <83103105+Sanu7D0@users.noreply.github.com>
2022-01-04 17:15:16 +0900

    feat: Fix thumbnail issue

--- app/src/main/java/com/flowcamp/tab/ui/main/PhoneFragment.java
+++ app/src/main/java/com/flowcamp/tab/ui/main/PhoneFragment.java
@@ -2,6 +2,7 @@ package com.flowcamp.tab.ui.main;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
@@ -10,10 +11,12 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.ImageDecoder;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.CommonDataKinds;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,6 +32,7 @@ import com.flowcamp.tab.PhoneListViewAdapter;
 import com.flowcamp.tab.R;
 import com.flowcamp.tab.databinding.FragmentPhoneBinding;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -64,6 +68,7 @@ public class PhoneFragment extends Fragment {
         pageViewModel.setIndex(index);
     }
 
+    @SuppressLint("WrongThread")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -82,7 +87,7 @@ public class PhoneFragment extends Fragment {
 
         String sortOrder = CommonDataKinds.Phone.DISPLAY_NAME + " COLLATE LOCALIZED ASC";
         String[] PHOTO_BITMAP_PROJECTION = new String[] {
-                ContactsContract.CommonDataKinds.Photo.PHOTO
+                CommonDataKinds.Photo.PHOTO
         };
 
         try {
@@ -90,23 +95,9 @@ public class PhoneFragment extends Fragment {
             if (cursor.moveToFirst()) {
                 do {
                     @SuppressLint("Range") long id = cursor.getLong(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-                    Uri photoUri = ContentUris.withAppendedId(ContactsContract.Data.CONTENT_URI, id);
                     Bitmap photo = null;
-//                    photo =
 
-                    /*Cursor photoCursor = context.getContentResolver().
-                            query(photoUri, PHOTO_BITMAP_PROJECTION, null, null, null);
-
-                    try {
-                        if (photoCursor.moveToFirst()) {
-                            final byte[] photoBytes = photoCursor.getBlob(0);
-                            if (photoBytes != null) {
-                                photo = BitmapFactory.decodeByteArray(photoBytes, 0, photoBytes.length);
-                            }
-                        }
-                    } finally {
-                        photoCursor.close();
-                    }*/
+                    photo = getFacebookPhoto(context, cursor.getString(2));
 
                     Phone phone = new Phone(
                             (int) cursor.getLong(0),
@@ -155,6 +146,37 @@ public class PhoneFragment extends Fragment {
         );
 
         return rootView;
+    }
+
+    public Bitmap getFacebookPhoto(Context context, String phoneNumber) {
+        Uri phoneUri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
+        Uri photoUri = null;
+        ContentResolver cr = context.getContentResolver();
+        Cursor contact = cr.query(phoneUri,
+                new String[] { ContactsContract.Contacts._ID }, null, null, null);
+
+        Bitmap defaultPhoto = null;
+        if (contact.moveToFirst()) {
+            @SuppressLint("Range") long userId = contact.getLong(contact.getColumnIndex(ContactsContract.Contacts._ID));
+            photoUri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, userId);
+
+        }
+        else {
+//            Bitmap defaultPhoto = BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_report_image);
+            return defaultPhoto;
+        }
+        if (photoUri != null) {
+            InputStream input = ContactsContract.Contacts.openContactPhotoInputStream(
+                    cr, photoUri);
+            if (input != null) {
+                return BitmapFactory.decodeStream(input);
+            }
+        } else {
+//            Bitmap defaultPhoto = BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_report_image);
+            return defaultPhoto;
+        }
+//        Bitmap defaultPhoto = BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_report_image);
+        return defaultPhoto;
     }
 
     public void addContact(String name, String number) {
